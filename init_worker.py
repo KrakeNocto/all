9881491940f4f567e2a5e7c78c4d00906f35262d
KrakeNocto <octo2kraken@gmail.com>
2024-09-19 11:02:19 +0300

    Update init_worker.py

--- init_worker.py
+++ init_worker.py
@@ -77,7 +77,24 @@ AVAILABLE_10_TOPIC_ARGS = {
     'TRU':'TRUUSDT',
     'POLYX':'POLYXUSDT',
     'MKR':'MKRUSDT',
-    'GMT':'GMTUSDT' 
+    'GMT':'GMTUSDT',
+    'PYTH':''PYTHUSDT',
+    'AVAX':''AVAXUSDT',
+    'CHR':''CHRUSDT',
+    'HIFI':''HIFIUSDT',
+    'LTO':''LTOUSDT',
+    'ARPA':''ARPAUSDT',
+    'ASTR':''ASTRUSDT',
+    'ATOM':''ATOMUSDT',
+    'AXL':''AXLUSDT',
+    'BB':''BBUSDT',
+    'CELO':''CELOUSDT',
+    'CFX':''CFXUSDT',
+    'CHZ':''CHZUSDT',
+    'COMBO':''COMBOUSDT',
+    'DCR':''DCRUSDT',
+    'EGLD':''EGLDUSDT',
+    'EOS':''EOSUSDT'
 }
 
 AVAILABLE_10_TOPIC_ARGS_LIST = list(AVAILABLE_10_TOPIC_ARGS.keys())
