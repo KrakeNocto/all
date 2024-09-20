8917bb281b15902867a5c3e46cafdc985edece6a
KrakeNocto <octo2kraken@gmail.com>
2024-09-20 17:47:48 +0300

    Create allora_0.5.0_upd.sh

---
+++ allora_0.5.0_upd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+systemctl stop allorad
+
+wget https://github.com/allora-network/allora-chain/releases/download/v0.5.0/allorad_linux_amd64
+
+rm /root/.allorad/cosmovisor/upgrades/v0.4.0/bin/allorad
+
+mv allorad_linux_amd64 /root/.allorad/cosmovisor/upgrades/v0.4.0/bin/allorad
+
+chmod +x /root/.allorad/cosmovisor/upgrades/v0.4.0/bin/allorad
+
+systemctl restart allora && journalctl -fu allora
