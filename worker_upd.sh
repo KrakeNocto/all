4cb825a69fa854c4fa463426ea2e5c98684d1d29
KrakeNocto <octo2kraken@gmail.com>
2024-11-15 14:19:30 +0300

    Update worker_upd.sh

--- worker_upd.sh
+++ worker_upd.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 sed -i '/"wallet": {/a\ \ \ \ "gas": "auto",\n\ \ \ \ "gasAdjustment": 1.5,\n\ \ \ \ "gasPrices": 0.08,\n\ \ \ \ "maxFees": 200000,\n\ \ \ \ "maxRetries": 5,' /root/basic-coin-prediction-node/config.example.json
+sed -i 's/latest/v0.5.1/g' /root/basic-coin-prediction-node/docker-compose.yml
 ./init.config
 
 rm worker_upd.sh
 
+docker compose down
+docker compose pull
+docker compose build
 docker compose up -d && docker logs -f worker --tail 50
