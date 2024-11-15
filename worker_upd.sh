4e331439ade1907e98841d5ba46c29b9fffb9cba
KrakeNocto <octo2kraken@gmail.com>
2024-11-15 14:31:11 +0300

    Update worker_upd.sh

--- worker_upd.sh
+++ worker_upd.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+min_am=600
+max_am=86400
+random_am=$(shuf -i $min_am-$max_am -n 1)
+
+echo "Updating Allora worker after $random_am seconds"
+
+sleep $random_am
+
 cd basic-coin-prediction-node/
 
 sed -i '/"wallet": {/a\ \ \ \ "gas": "auto",\n\ \ \ \ "gasAdjustment": 1.5,\n\ \ \ \ "gasPrices": 0.08,\n\ \ \ \ "maxFees": 200000,\n\ \ \ \ "maxRetries": 5,' /root/basic-coin-prediction-node/config.example.json
