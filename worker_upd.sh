7166d3f5fb17fc6624be49f17004a329c02daf84
KrakeNocto <octo2kraken@gmail.com>
2024-12-08 15:47:44 +0300

    Update worker_upd.sh

--- worker_upd.sh
+++ worker_upd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 min_am=600
-max_am=86400
+max_am=61200
 random_am=$(shuf -i $min_am-$max_am -n 1)
 
 echo "Updating Allora worker after $random_am seconds"
@@ -10,13 +10,19 @@ sleep $random_am
 
 cd basic-coin-prediction-node/
 
-sed -i '/"wallet": {/a\ \ \ \ "gas": "auto",\n\ \ \ \ "gasAdjustment": 1.5,\n\ \ \ \ "gasPrices": 0.08,\n\ \ \ \ "maxFees": 200000,\n\ \ \ \ "maxRetries": 5,' /root/basic-coin-prediction-node/config.json
-sed -i 's/latest/v0.5.1/g' /root/basic-coin-prediction-node/docker-compose.yml
+sed -i '/"wallet": {/a\ \ \ \ "gas": "auto",\n\ \ \ \ "gasAdjustment": 2,\n\ \ \ \ "gasPrices": "10",\n\ \ \ \ "maxFees": 25000000,\n\ \ \ \ "maxRetries": 5,' /root/basic-coin-prediction-node/config.json
+sed -i 's/"inferenceEntrypointName": "api-worker-reputer"/"inferenceEntrypointName": "apiAdapter"/' /root/basic-coin-prediction-node/config.json
+sed -i '/"wallet": {/a \
+  "retryDelay": 3,\n\
+  "accountSequenceRetryDelay": 5,\n\
+  "blockDurationEstimated": 10,\n\
+  "windowCorrectionFactor": 0.8,' /root/basic-coin-prediction-node/config.json
+sed -i 's/v0.5.1/v0.7.0/g' /root/basic-coin-prediction-node/docker-compose.yml
+sed -i 's/"nodeRpc": "[^"]*"/"nodeRpc": "https:\/\/rpc.ankr.com\/allora_testnet"/' /root/basic-coin-prediction-node/config.json
 ./init.config
 
 rm worker_upd.sh
 
 docker compose down
 docker compose pull
-docker compose build
 docker compose up -d && docker logs -f worker --tail 50
